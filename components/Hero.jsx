d3d01202252486f20e5d6dbac122a42045b75ee9
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-23 16:24:41 +0300

    done the hero section at md screen

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -7,26 +7,37 @@ import HeroInfo from "@/public/HeroCardInfo.svg";
 const Hero = () => {
   return (
     <section className="py-[30px] bg-[#2B2B2B]">
-      <div className="md:flex flex-wrap mx-[30px]">
-        <div className="font-sans text-white">
-          <h4 className="text-[28px] font-semibold">
-            Discover digital art & Collect NFTs
-          </h4>
-          <h2 className="text-base mt-2">
-            NFT marketplace UI created with Anima for Figma. Collect, buy and
-            sell art from more than 20k NFT artists.
-          </h2>
+      <div className="md:flex flex-wrap gap-x-10 items-center justify-center mx-[30px]">
+        <div className="max-w-[340px]">
+          <div className="font-sans text-white">
+            <h4 className="text-[28px] md:text-[38px] font-semibold">
+              Discover digital art & Collect NFTs
+            </h4>
+            <h2 className="text-base mt-2">
+              NFT marketplace UI created with Anima for Figma. Collect, buy and
+              sell art from more than 20k NFT artists.
+            </h2>
+          </div>
+          <div className="bg-[#A259FF] px-[50px] py-5 rounded-3xl justify-center items-center text-center md:block hidden mt-10">
+            <button className="text-white font-semibold font-sans gap-4 flex">
+              <Image src={RocketIcon} alt="RocketLaunch Icon" className="w-5" />
+              Get Started
+            </button>
+          </div>
+          <div className="mt-10 md:block hidden">
+            <Image src={HeroInfo} alt="Hero Info" />
+          </div>
         </div>
         <div className="my-10">
-          <Image src={HeroCard} alt="Hero Card" />
+          <Image src={HeroCard} alt="Hero Card" className="w-[330px]" />
         </div>
-        <div className="bg-[#A259FF] px-[50px] py-5 rounded-3xl justify-center items-center text-center flex">
+        <div className="bg-[#A259FF] px-[50px] py-5 rounded-3xl justify-center items-center text-center flex md:hidden">
           <button className="text-white font-semibold font-sans gap-4 flex">
             <Image src={RocketIcon} alt="RocketLaunch Icon" className="w-5" />
             Get Started
           </button>
         </div>
-        <div className="mt-10">
+        <div className="mt-10 block md:hidden">
           <Image src={HeroInfo} alt="Hero Info" />
         </div>
       </div>
