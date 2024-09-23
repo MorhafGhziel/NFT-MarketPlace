aafc3dce81732933eced8b0bdae76efcaa1ddb08
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-23 13:50:49 +0300

    added padding and the navlogo

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import NavLogo from "@/public/NavLogo.svg";
+import Image from "next/image";
 
 const Navigation = () => {
-  return <div>Navigation</div>;
+  return (
+    <header>
+      <div className="px-[50px] py-[20px] bg-[#2B2B2B]">
+        <div>
+          <Image src={NavLogo} alt="Nav Logo" />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Navigation;
