c2e76f841f5d814ca4f169d315a7caa88babbb50
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-23 16:04:37 +0300

    done the button

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import HeroCard from "@/public/HeroCard.svg";
 import Image from "next/image";
+import RocketIcon from "@/public/RocketLaunch.svg";
 
 const Hero = () => {
   return (
     <section className="py-[30px] bg-[#2B2B2B]">
-      <div className="md:flex  flex-wrap mx-[30px]">
+      <div className="md:flex flex-wrap mx-[30px]">
         <div className="font-sans text-white">
           <h4 className="text-[28px] font-semibold">
             Discover digital art & Collect NFTs
@@ -17,6 +18,12 @@ const Hero = () => {
         </div>
         <div className="my-10">
           <Image src={HeroCard} alt="Hero Card" />
+        </div>
+        <div className="bg-[#A259FF] px-[50px] py-5 rounded-3xl justify-center items-center text-center flex">
+          <button className="text-white font-semibold font-sans gap-4 flex">
+            <Image src={RocketIcon} alt="RocketLaunch Icon" className="w-5" />
+            Get Started
+          </button>
         </div>
       </div>
     </section>
