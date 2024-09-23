f78248c2a9842a6218473cb433a2d514453e1a5b
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-23 20:27:55 +0300

    new edits on hero section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -9,11 +9,11 @@ const Hero = () => {
     <section className="py-[30px] bg-[#2B2B2B]">
       <div className="md:flex gap-x-10 items-center justify-center mx-[30px]">
         <div className="max-w-[340px] md:max-w-[330px] lg:max-w-[440px] lg:leading-none m-auto md:m-0">
-          <div className="font-sans text-white m-auto">
-            <h4 className="text-[28px] md:text-[38px] lg:text-[67px] font-semibold">
+          <div className="font-sans m-auto">
+            <h4 className="text-[28px] md:text-[38px] text-white lg:text-[67px] font-semibold">
               Discover digital art & Collect NFTs
             </h4>
-            <h2 className="text-base mt-2 lg:mt-6 lg:text-[22px] leading-8">
+            <h2 className="text-base mt-2 lg:mt-6 lg:text-[22px] md:leading-8 text-gray-200">
               NFT marketplace UI created with Anima for Figma. Collect, buy and
               sell art from more than 20k NFT artists.
             </h2>
@@ -36,7 +36,7 @@ const Hero = () => {
           <Image
             src={HeroCard}
             alt="Hero Card"
-            className="w-[315px] md:w-[330px] lg:w-[540px] m-auto"
+            className="w-[340px] md:w-[330px] lg:w-[540px] m-auto"
           />
         </div>
         <div className="bg-[#A259FF] px-[50px] py-5 rounded-3xl justify-center items-center text-center flex md:hidden max-w-fit m-auto">
