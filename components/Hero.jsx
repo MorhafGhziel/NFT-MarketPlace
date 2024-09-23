2fc9ef51f24608c7e82aa4a1011448d009fe9553
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-23 15:44:45 +0300

    finished the texts

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="py-[30px] bg-[#2B2B2B]">
+      <div className="flex-wrap ml-[30px]">
+        <div className="font-sans text-white">
+          <h4 className="text-[28px] font-semibold">
+            Discover digital art & Collect NFTs
+          </h4>
+          <h2 className="text-base mt-2">
+            NFT marketplace UI created with Anima for Figma. Collect, buy and
+            sell art from more than 20k NFT artists.
+          </h2>
+        </div>
+        <div></div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
