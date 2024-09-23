d0b18e461af8acec47ccf508116aecebc3eea218
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-23 20:47:22 +0300

    added TrendingCollection to page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero";
 import Navigation from "@/components/Navigation";
+import TrendingCollection from "@/components/TrendingCollection";
 
 export default function Home() {
   return (
     <>
       <Navigation />
       <Hero />
+      <TrendingCollection />
     </>
   );
 }
