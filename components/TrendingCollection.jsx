be927cdff0bc1ace64f7696811b5064e652b6621
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-24 13:35:02 +0300

    imported the images

--- components/TrendingCollection.jsx
+++ components/TrendingCollection.jsx
@@ -1,5 +1,20 @@
 import React from "react";
 
+import Image from "next/image";
+import Prim1 from "@/public/Primary1.svg";
+import Prim2 from "@/public/Primary2.svg";
+import Prim3 from "@/public/Primary3.svg";
+import sec1 from "@/public/Sec1.svg";
+import sec11 from "@/public/Sec11.svg";
+import sec2 from "@/public/Sec2.svg";
+import sec22 from "@/public/Sec22.svg";
+import sec3 from "@/public/Sec3.svg";
+import sec33 from "@/public/Sec33.svg";
+import Number from "@/public/Number.svg";
+import Profile1 from "@/public/Profile1.svg";
+import Profile2 from "@/public/Profile2.svg";
+import Profile3 from "@/public/Profile3.svg";
+
 const TrendingCollection = () => {
   return (
     <section className="py-[30px] px-[50px] bg-[#2B2B2B]">
