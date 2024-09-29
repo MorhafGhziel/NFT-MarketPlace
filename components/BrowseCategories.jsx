699c36be3e0a911ce6ed51104ec383d965f028d9
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-29 23:17:06 +0300

    imported the images

--- components/BrowseCategories.jsx
+++ components/BrowseCategories.jsx
@@ -1,4 +1,12 @@
 import React from "react";
+import Image from "next/image";
+import CategoryCard from "@/public/CategoryCard";
+import CategoryCard2 from "@/public/CategoryCard2";
+import CategoryCard3 from "@/public/CategoryCard3";
+import CategoryCard4 from "@/public/CategoryCard4";
+import CategoryCard5 from "@/public/CategoryCard5";
+import CategoryCard6 from "@/public/CategoryCard6";
+import CategoryCard7 from "@/public/CategoryCard7";
 
 const BrowseCategories = () => {
   return (
