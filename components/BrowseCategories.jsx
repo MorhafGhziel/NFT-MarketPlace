4ab015bbe2fae0d0299c973d9ed59708d6d46e10
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-10-01 16:18:50 +0300

    done categories at all screens

--- components/BrowseCategories.jsx
+++ components/BrowseCategories.jsx
@@ -10,6 +10,14 @@ import CategoryCardMd5 from "@/public/CategoryCardMd5.svg";
 import CategoryCardMd6 from "@/public/CategoryCardMd6.svg";
 import CategoryCard7 from "@/public/CategoryCard7.svg";
 import CategoryCard8 from "@/public/CategoryCard8.svg";
+import CategoryCardLg1 from "@/public/CategoryCardLg1.svg";
+import CategoryCardLg2 from "@/public/CategoryCardLg2.svg";
+import CategoryCardLg3 from "@/public/CategoryCardLg3.svg";
+import CategoryCardLg4 from "@/public/CategoryCardLg4.svg";
+import CategoryCardLg5 from "@/public/CategoryCardLg5.svg";
+import CategoryCardLg6 from "@/public/CategoryCardLg6.svg";
+import CategoryCardLg7 from "@/public/CategoryCardLg7.svg";
+import CategoryCardLg8 from "@/public/CategoryCardLg8.svg";
 
 const BrowseCategories = () => {
   // Group images into an array with their src and alt attributes
@@ -23,6 +31,7 @@ const BrowseCategories = () => {
     { id: 7, src: CategoryCard7, alt: "Category 7" },
     { id: 8, src: CategoryCard8, alt: "Category 8" },
   ];
+
   const categoriesMd = [
     { id: 1, src: CategoryCard, alt: "Category 1" },
     { id: 2, src: CategoryCard2, alt: "Category 2" },
@@ -32,6 +41,17 @@ const BrowseCategories = () => {
     { id: 6, src: CategoryCardMd6, alt: "Category 6" },
     { id: 7, src: CategoryCard7, alt: "Category 7" },
     { id: 8, src: CategoryCard8, alt: "Category 8" },
+  ];
+
+  const categoriesLg = [
+    { id: 1, src: CategoryCardLg1, alt: "Category 1" },
+    { id: 2, src: CategoryCardLg2, alt: "Category 2" },
+    { id: 3, src: CategoryCardLg3, alt: "Category 3" },
+    { id: 4, src: CategoryCardLg4, alt: "Category 4" },
+    { id: 5, src: CategoryCardLg5, alt: "Category 5" },
+    { id: 6, src: CategoryCardLg6, alt: "Category 6" },
+    { id: 7, src: CategoryCardLg7, alt: "Category 7" },
+    { id: 8, src: CategoryCardLg8, alt: "Category 8" },
   ];
 
   return (
@@ -51,6 +71,15 @@ const BrowseCategories = () => {
       <div className="flex flex-wrap justify-center gap-6 lg:hidden">
         {/* Map over categories array to render images */}
         {categoriesMd.map((category) => (
+          <div key={category.id}>
+            <Image src={category.src} alt={category.alt} />
+          </div>
+        ))}
+      </div>
+
+      <div className="lg:grid lg:grid-cols-4 max-w-5xl mx-auto justify-center gap-6 hidden">
+        {/* Map over categories array to render images */}
+        {categoriesLg.map((category) => (
           <div key={category.id}>
             <Image src={category.src} alt={category.alt} />
           </div>
