68324b4b03a5bd1c2f3ebdedb9ca6b8a70a3cb75
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-10-01 16:44:33 +0300

    added title

--- components/DiscovermoreNFTs.jsx
+++ components/DiscovermoreNFTs.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const DiscovermoreNFTs = () => {
-  return <div>DiscovermoreNFTs</div>;
+  return (
+    <section className="py-[40px] md:py-[30px] lg:py-[90px] bg-[#2B2B2B]">
+      <div className="text-white">
+        <h2 className="lg:text-[38px] text-[28px] md:text-[28px] font-semibold">
+          Top creators
+        </h2>
+        <h2 className="text-[16px] lg:text-[22px] text-gray-200 max-w-[350px] md:max-w-[400px] lg:max-w-full font-sans tracking-wider mt-2">
+          Checkout Top Rated Creators on the NFT Marketplace
+        </h2>
+      </div>
+    </section>
+  );
 };
 
 export default DiscovermoreNFTs;
